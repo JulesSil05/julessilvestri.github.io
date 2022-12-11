2a2f4a451d8e48ca94e8af7a8f4c5d75dc7a607f
jules <jules.silvestri@etu.univ-smb.fr>
2022-12-11 13:37:57 +0100

    first version

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let wokingTaskItem = document.querySelector("#working_circle")
+let projectPictures = document.querySelectorAll(".project-picture")
+let progressBar = document.querySelector("#progress-bar")
+let scrollPath = document.querySelector("#scroll-path")
+let totalHeight = document.body.scrollHeight - window.innerHeight
+const menuHamburger = document.querySelector('.menu-hamburger')
+const navLinks = document.querySelector('.nav-links')
+let linkNavs = document.querySelectorAll('.link-nav')
+
+menuHamburger.addEventListener('click', function(){
+    navLinks.classList.toggle('mobile-menu')
+})
+
+linkNavs.forEach(link => {
+    link.addEventListener('click', function(){
+        navLinks.classList.remove('mobile-menu')
+    })
+});
+
+window.onscroll = function(){
+    let progressHeight = window.pageYOffset * scrollPath.clientHeight / totalHeight
+    progressBar.style.height = progressHeight + "px"
+}
+
+setInterval(function(){
+    wokingTaskItem.classList.toggle('hidden')
+},1000)
+
